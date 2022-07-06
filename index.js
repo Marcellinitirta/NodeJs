96dd553388bd253c8f6c84d2972f942de8dfdb39
Marcellini <marcellinitirta@gmail.com>
2022-07-06 23:44:37 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+
+// let angka_pertama = prompt ("input angka pertama");
+// let angka_kedua = prompt ("input angka_kedua");
+// let kuadrat = Math.pow (angka_pertama, angka_kedua);
+// let pembagian = angka_pertama/angka_kedua
+
+// alert(`hasil pembagian: ${pembagian} dan hasil kuadrat: ${kuadrat}`)
+
+
+
+// let time = new Date()
+// time.setDate(time.getDate() + -1);
+// alert(time+ "adalah hari kemarin");
+
+// time.setDate(time.getDate() + 1);
+// alert(time + "adalah hari ini");
+
+// time.setDate (time.getDate() + 1);
+// alert(time + "adalah hari esok");
+
+
+let str = prompt("masukkan sebuah kata");
+alert("panjang karakter: " + str.length + "karakter");
+
+
